c41592dcff2e4ef8abf2a2fe72800f7788d891c7
Jorge Casarez <jorge.alfonso.casarez.palafox@uabc.edu.mx>
2023-05-29 19:12:44 -0700

    a

--- components/ResumenProducto.js
+++ components/ResumenProducto.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { formatearDinero } from '../helpers';
 
 const ResumenProducto = ({producto}) => {
     return (
@@ -11,7 +12,30 @@ const ResumenProducto = ({producto}) => {
             src={`/assets/img/${producto.imagen}.jpg`}>
 
           </Image>
-        </div>
+          </div>
+          <div className='md:w-5/6'>
+            <p className='text-3xl font-bold'>
+              Cantidad: {producto.cantidad}
+            </p>
+            <p className='text-xl font-bold mt-2 text-amber-500'>
+              Precio: {formatearDinero(producto.precio)}
+            </p>
+            <p className='text-sm font-bold mt-2'>
+              SubTotal: {formatearDinero(producto.precio * producto.cantidad)}
+            </p>
+          </div>
+          <div>
+            <button
+            type='button'
+            className='bg-sky-700 flex px-5 py-2 text-white rounded-md font-bold uppercase shadow-md w-full mt-3'>
+              Editar
+            </button>
+            <button
+            type='button'
+            className='bg-red-700 flex px-5 py-2 text-white rounded-md font-bold uppercase shadow-md w-full mt-3'>
+              Eliminar
+            </button>
+          </div>
       </div>
     )
   }
